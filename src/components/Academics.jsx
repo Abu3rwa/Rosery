4fc346166528972b0af940875c01494d4290857c
Abu3rwa <Abu3rwa.abdulhafeez@gmail.com>
2023-10-05 10:14:01 +0200

    INIT

--- src/components/Academics.jsx
+++ src/components/Academics.jsx
@@ -1,30 +1,113 @@
 import "./academics.css";
+import CheckIcon from "@mui/icons-material/Check";
+import Cross from "@mui/icons-material/Check";
+import { useEffect, useState } from "react";
+function Academics({ profile }) {
+  const [active, setActive] = useState(localStorage.getItem("active"));
 
-function Academics() {
   return (
-    <div className="academics col-12 col-md-9 ">
-      <h1 className="mt-3 tx-dark    text-center">Academic</h1>
-      <ul>
-        <h3 className="dark-rose">unsolved Homeworks</h3>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-      </ul>
-      <ul>
-        <h3 className="dark-rose">Finished</h3>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-      </ul>
-      <ul>
-        <h3 className="dark-rose">Homeworks to be correct</h3>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-        <li>homeword 1</li>
-      </ul>
+    <div className="academics  col-12 col-md-9 row ">
+      {/* <h1 className="mt-3 tx-dark    text-center">Academic</h1> */}
+      <div className="subject col-12">
+        <ul className="list">
+          {profile.subjects?.map((subject, index) => (
+            <li
+              key={subject}
+              className={subject === active ? "active" : ""}
+              onClick={() => {
+                setActive(subject);
+              }}
+            >
+              {" "}
+              {subject}
+            </li>
+          ))}
+        </ul>
+        <ul className="homeworks-list">
+          <h3 className="dark-rose">today's Homeworks</h3>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <span className="new"></span>
+          </li>
+        </ul>
+        <ul className="homeworks-list">
+          <h3 className="dark-rose">Homeworks to be correct</h3>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="to-be-corrected" />
+          </li>
+        </ul>
+        <ul className="homeworks-list">
+          <h3 className="dark-rose">Finished</h3>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+          <li>
+            <span>What are you?</span>
+            <span>8/10/2013</span>
+            <span>9/10/2013</span> <CheckIcon className="finished" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/screens/StudentProfile.jsx
+++ src/screens/StudentProfile.jsx
@@ -6,9 +6,11 @@ import Academics from "../components/Academics";
 import { axiosInstance } from "../config/axiosInstance";
 import logo from "../assets/logo.jpg";
 import "./studentProfile.css";
+import { Avatar } from "@mui/material";
 function StudentProfile() {
+  const [active, setActive] = useState([]);
   const [profile, setProfile] = useState([]);
-  console.log(localStorage.getItem("token"));
+
   function loadProfile() {
     axiosInstance
       .get("/auth/me", {
@@ -21,19 +23,60 @@ function StudentProfile() {
       .then((profile) => {
         console.log(profile.data);
         profile.data && setProfile(profile.data);
+        localStorage.setItem("active", profile?.subjects[0]);
       })
       .catch((error) => {
         console.log(error);
       });
   }
   useEffect(() => {
+    setActive(localStorage.getItem("active"));
+
     loadProfile();
-  }, []);
+  }, [active]);
   return (
     <>
       {profile && (
         <div className="profile row">
-          <Academics />
+          <div className="main-info col-12 col-md-3">
+            <button
+              className="btn btn-danger logout btn-sm"
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.replace("/");
+              }}
+            >
+              Logout
+            </button>
+            {/* <img src={logo} alt="" />
+             */}
+            <Avatar className="img">{profile?.name?.split(" ")[0]}</Avatar>
+
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Name | </span>
+              {profile.name}
+            </h5>
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Grade | </span>
+              {profile.grade}
+            </h5>
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Address | </span>
+              {profile.address}
+            </h5>
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Age |</span> {profile.age}
+            </h5>
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Email |</span> {profile.email}
+            </h5>
+            <h5 className="tx-dark">
+              <span className="md-rose  ">Phone |</span> {profile.phoneNumber}
+            </h5>
+
+            {/* <h5>{profile.grade}</h5> */}
+          </div>
+          <Academics profile={profile} active={active} />
         </div>
       )}
     </>
